67841e57f9f750bd4b6f9dd7634da8a92e5f9292
Majdinho <mario.majdandzic@fer.hr>
2013-10-22 15:55:32 +0200

    Removed default Rails page

--- hashes_app.rb
+++
@@ -1,9 +0,0 @@
-person1 = { :first => "Mario" , :last => "Majdandzic" }
-person2 = { :first => "Arijana" , :last => "Stajcic" }
-person3 = { :first => "Antonio" , :last => "Majdandzic" }
-
-params = {}
-
-params[:father] = person1
-params[:mother] = person2
-params[:child] = person3
