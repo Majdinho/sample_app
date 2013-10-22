5c7b4c05266b2d5e81d5a0fd5b1fdbc262fe4dac
Majdinho <mario.majdandzic@fer.hr>
2013-10-22 13:42:39 +0200

    Edited some files

---
+++ hashes_app.rb
@@ -0,0 +1,9 @@
+person1 = { :first => "Mario" , :last => "Majdandzic" }
+person2 = { :first => "Arijana" , :last => "Stajcic" }
+person3 = { :first => "Antonio" , :last => "Majdandzic" }
+
+params = {}
+
+params[:father] = person1
+params[:mother] = person2
+params[:child] = person3
